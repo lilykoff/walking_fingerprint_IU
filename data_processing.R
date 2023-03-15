6187fae8b1a2a9f37c6115b67b2389f573d0234c
lilykoff <koffmanlily@gmail.com>
2023-03-15 16:54:54 -0400

    updated sensitivity analysis

--- data_processing.R
+++ data_processing.R
@@ -1,9 +1,14 @@
+library(purrr)
+
 data_path <- "./data"
-if(!dir.exists(data_path)) dir.create(data_path)
+if (!dir.exists(data_path))
+  dir.create(data_path)
 acc_dat.dir <- file.path(data_path, "IU_walking_driving_climbing")
-acc_dat.zip <- file.path(data_path, "IU_walking_driving_climbing.zip")
-acc_dat.zip.url <- "https://www.dropbox.com/s/pf5l2ki9t2ae2df/IU_walking_driving_climbing.zip?dl=1"
-if (!dir.exists(acc_dat.dir) & !file.exists(acc_dat.zip)){
+acc_dat.zip <-
+  file.path(data_path, "IU_walking_driving_climbing.zip")
+acc_dat.zip.url <-
+  "https://www.dropbox.com/s/pf5l2ki9t2ae2df/IU_walking_driving_climbing.zip?dl=1"
+if (!dir.exists(acc_dat.dir) & !file.exists(acc_dat.zip)) {
   ## Download .zip file
   download.file(acc_dat.zip.url, acc_dat.zip)
   ## Unzip into data/ directory
@@ -11,43 +16,48 @@ if (!dir.exists(acc_dat.dir) & !file.exists(acc_dat.zip)){
   ## Remove .zip file
   file.remove(acc_dat.zip)
 }
-files <- list.files("/Users/lilykoffman/Documents/walking_fingerprint_IU/data/IU_walking_driving_climbing/raw_accelerometry_data",
-                    full.names = T)
-read_files <- function(x){
-  readr::read_csv(x, show_col_types = F) %>% mutate(
-    ID = sub(".csv.*", "",sub(".*id", "", x))
+
+# get names of files 
+files <-
+  list.files(
+    "/Users/lilykoffman/Documents/walking_fingerprint_IU/data/IU_walking_driving_climbing/raw_accelerometry_data",
+    full.names = T
   )
+# function to read files and create ID column 
+read_files <- function(x) {
+  readr::read_csv(x, show_col_types = F) %>% mutate(ID = sub(".csv.*", "", sub(".*id", "", x)))
 }
 
-library(purrr)
-df_all <- files %>% map_dfr(read_files) 
-# filter to just walking
-df_all_filtered <- df_all  %>%
-  filter(activity==1) %>% # filtered to just walking 
-  dplyr::mutate(time_g = time_s - min(time_s), # getting time in seconds from start 
-                time_g = floor(time_g)) %>% 
-  group_by(ID) %>% 
-  mutate(time_g = time_g-min(time_g),
-         j = 1:n()) %>% 
-  group_by(ID, time_g) %>% 
-  dplyr::mutate(n_g = n(),
-                time_s_g=1:n()) %>% 
-  ungroup() %>% 
-  filter(n_g == 100) %>% dplyr::select(-c(n_g, j)) %>% rename("J" = "time_g", t = "time_s_g") %>%
-  mutate(
-    signal_lw = sqrt(lw_x^2 +lw_y^2+lw_z^2),
-    signal_lh = sqrt(lh_x^2 +lh_y^2+lh_z^2),
-    signal_la = sqrt(la_x^2 +la_y^2+la_z^2),
-    signal_ra = sqrt(ra_x^2 +ra_y^2+ra_z^2),
-  ) %>% mutate(ID2 = as.numeric(as.factor(ID)))
+# read in all files and row bind into df 
+df_all <- files %>% map_dfr(read_files)
 
-# fixing weird J 
+
+df_all_filtered <- df_all  %>%
+  filter(activity == 1) %>% # filtered to just walking
+  mutate(time_g = time_s - min(time_s), # starting time from 0 
+                time_g = floor(time_g)) %>%
+  group_by(ID) %>%
+  mutate(time_g = time_g - min(time_g)) %>%
+  group_by(ID, time_g) %>% # group by ID and second, get number of observations per second 
+  mutate(n_g = n(),
+                time_s_g = 1:n()) %>%
+  ungroup() %>%
+  filter(n_g == 100) %>% # keeping just seconds with whole second of observations 
+ dplyr::select(-n_g) %>% 
+  mutate(
+    signal_lw = sqrt(lw_x ^ 2 + lw_y ^ 2 + lw_z ^ 2),
+    signal_lh = sqrt(lh_x ^ 2 + lh_y ^ 2 + lh_z ^ 2),
+    signal_la = sqrt(la_x ^ 2 + la_y ^ 2 + la_z ^ 2),
+    signal_ra = sqrt(ra_x ^ 2 + ra_y ^ 2 + ra_z ^ 2),
+  ) %>% mutate(ID2 = as.numeric(as.factor(ID))) # making ID numeric 
+
 df_all_final <- df_all_filtered %>% group_by(ID2) %>% mutate(
   rownum = row_number(),
-  time_s_2 = rownum/100,
-  J_new = ceiling(time_s_2)
-) %>% ungroup() %>% dplyr::select(-c(J, t))
+  time_s_2 = rownum / 100,
+  J = ceiling(time_s_2)
+) %>% ungroup() 
 
 
 
 write.csv(df_all_final, "df_all.csv")
+
--- pipeline.R
+++ pipeline.R
@@ -1,80 +1,42 @@
 
 library(tidyverse)
 library(readr)
+
+# steps to replicate analysis
+# run data processing file to get raw processed accelerometry data
+# load all functions in functions.R file 
+# read in 
 df_all <- read_csv("df_all.csv", col_types = cols(...1 = col_skip())) 
 
-
-
-subjects <- unique(df_all$ID2)
+# vector of subjects
+subjects <- unique(df_all $ID2)
+# vector of time lags 
 time_lags <- seq(15, 90, 15)
+# grid cell size 
 gcell_size <- 0.25
+# location - could use other body parts instead 
 location <- "lw"
+# get grid cell data 
 all_data <- map_dfr_progress(.x = subjects, .f = get_grid_data,
                     time_lags = time_lags, gcell_size = gcell_size, location = location, data = df_all)
+# save file to save time in the futur
+# write.csv(all_data, "all_data.csv")
 
+all_data <- read_csv("all_data.csv")[,-1]
+# get 200 seconds for training, 180 seconds for testing 
 data_train <- all_data %>% group_by(ID) %>% 
   mutate( maxJ = max(J)) %>% filter(J <= 200) %>% dplyr::select(-c(J, maxJ)) %>% ungroup()
 data_test <- all_data %>% group_by(ID) %>% 
   mutate( maxJ = max(J)) %>% filter(J > 200 & J <= 380) %>%  dplyr::select(-c(J, maxJ)) %>% ungroup()
 
-
+# get predicted values from logistic regression 
 all_predictions <- map_dfr_progress(.x = subjects, n_predictors=Inf, train = data_train,
                            test = data_test,threshold = 0.001,
                            .f = fit_model)
+
+# plot and summarie predictions 
 plot_preds(all_predictions)
 
-write.csv(all_data, "all_data.csv")
+summarize_preds(all_predictions)
 
 
-
-# stop here 
-gcell_size <- 0.15
-location <- "lw"
-all_data_gcell_0.15 <- map_dfr(.x = subjects, .f = get_grid_data,
-                    time_lags = time_lags, gcell_size = gcell_size, location = location, data = df_all)
-write.csv(all_data_gcell_0.15, "all_data_gc_0.15.csv")
-location <- "la"
-all_data_gcell_0.15_la <- map_dfr(.x = subjects, .f = get_grid_data,
-                               time_lags = time_lags, gcell_size = gcell_size, location = location, data = df_all)
-
-
-write.csv(all_data_gcell_0.15_la, "all_data_gc_0.15_la.csv")
-
-# write.csv(all_data, "all_data.csv")
-all_data <- read_csv("all_data.csv")[,-1]
-# split into training and testing based on percent 
-training_pct <- 0.75 
-threshold <- 0.001
-n_predictors=Inf
-
-data_train <- all_data %>% group_by(ID) %>% 
-  mutate( maxJ = max(J)) %>% filter(J <= maxJ*training_pct) %>% dplyr::select(-c(J, maxJ)) %>% ungroup()
-data_test <- all_data %>% group_by(ID) %>% 
-  mutate( maxJ = max(J)) %>% filter(J > maxJ*training_pct) %>%  dplyr::select(-c(J, maxJ)) %>% ungroup()
-subjects <- unique(all_data$ID)
-
-data_train <- all_data %>% group_by(ID) %>% 
-  mutate( maxJ = max(J)) %>% filter(J <= 200) %>% dplyr::select(-c(J, maxJ)) %>% ungroup()
-data_test <- all_data %>% group_by(ID) %>% 
-  mutate( maxJ = max(J)) %>% filter(J > 200) %>%  dplyr::select(-c(J, maxJ)) %>% ungroup()
-
-
-all_predictions <- map_dfr(.x = subjects, n_predictors=n_predictors, train = data_train,
-                           test = data_test,threshold = threshold,
-                           .f = fit_model)
-all_models <- map(.x = subjects, n_predictors=n_predictors, train = data_train,
-                           test = data_test,threshold = threshold,
-                           .f = extract_models)
-
-
-# print summary 
-summarize_preds(all_predictions = all_predictions)
-# store plot 
-g <- plot_preds(all_predictions = all_predictions)
-g
-
-training_pct <- 0.60
-all_predictions_30 <- map_dfr(.x = subjects, n_predictors=Inf, train = data_train,
-                           test = data_test,threshold = threshold,
-                           .f = fit_model)
-summarize_preds(all_predictions_30)
